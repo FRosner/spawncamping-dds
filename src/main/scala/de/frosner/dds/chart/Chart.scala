867b945773037f8dafe9966a0575a0b2280bb8f8
Frank Rosner <frank@fam-rosner.de>
2015-03-05 21:12:54 +0100

    #20 ScalaDoc for Chart

--- src/main/scala/de/frosner/dds/chart/Chart.scala
+++ src/main/scala/de/frosner/dds/chart/Chart.scala
@@ -3,6 +3,12 @@ package de.frosner.dds.chart
 import de.frosner.dds.core.Servable
 import spray.json._
 
+/**
+ * Class representing a <a href="http://c3js.org/reference.html">C3 chart</a>. It contains the id of the DOM object
+ * to be drawn into as well as the data to draw.
+ *
+ * @param data to plot in the chart
+ */
 case class Chart(data: Data) extends Servable {
 
   val servableType = "chart"
