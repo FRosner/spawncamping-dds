3da330d27a728de74d5a7f280560b5dc1c9a0a5d
Frank Rosner <frank@fam-rosner.de>
2015-03-05 21:28:39 +0100

    #20 ScalaDoc for ChartTypes

--- src/main/scala/de/frosner/dds/chart/ChartTypes.scala
+++ src/main/scala/de/frosner/dds/chart/ChartTypes.scala
@@ -3,8 +3,20 @@ package de.frosner.dds.chart
 import de.frosner.dds.chart.ChartTypeEnum.ChartType
 import spray.json.{JsObject, JsString}
 
+/**
+ * Collection of [[ChartType]]s with a corresponding JSON representation.
+ *
+ * @param types
+ */
 case class ChartTypes(types: Iterable[ChartType]) {
-  
+
+  /**
+   * Convert the chart types to a JSON object where each type is associated to the given label. Both iterables are
+   * zipped and then the labels are treated as keys for the corresponding [[ChartType]].
+   *
+   * @param labels of the [[ChartType]]s
+   * @return JSON object containing a mapping between labels and [[ChartType]]s
+   */
   def toJsonWithLabels(labels: Iterable[String]): JsObject = {
     require(labels.size == types.size)
     val labelTypePairs = labels.zip(types.map(t => JsString(t.toString)))
