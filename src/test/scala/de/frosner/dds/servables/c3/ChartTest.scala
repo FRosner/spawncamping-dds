8f986d0a04086f9be0c6e72eec215c175ee5f9c9
Frank Rosner <frank@fam-rosner.de>
2015-03-13 21:38:53 +0100

    #73 fix failing test

--- src/test/scala/de/frosner/dds/servables/c3/ChartTest.scala
+++ src/test/scala/de/frosner/dds/servables/c3/ChartTest.scala
@@ -5,11 +5,14 @@ import spray.json.{JsObject, JsString}
 
 class ChartTest extends FlatSpec with Matchers {
 
-  "A chart" should "have the correct JSON format" in {
+  "A chart" should "have the correct JSON format with default x-axis" in {
     val data = new DummyData("data", "1")
     Chart(data).contentAsJson shouldBe JsObject(
       ("bindto", JsString("#" + Chart.id)),
-      ("data", data.toJson)
+      ("data", data.toJson),
+      ("axis", JsObject(
+        ("x", XAxis.indexed.toJson)
+      ))
     )
   }
 
