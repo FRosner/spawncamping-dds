fd5a8928098f4e7f7ba084725615d1ac0cee28b9
milosk <milos.krstajic@gmail.com>
2015-03-19 17:39:13 +0100

    Parallel coordinates with table, linking and brushing
    - first version
    - improve styles and positioning in the next iteration

--- src/main/resources/ui/app/main.js
+++ src/main/resources/ui/app/main.js
@@ -26,7 +26,7 @@ function checkForUpdate() {
                 if (servable.type == "chart") {
                     generateSingleChart(servable.content)
                 } else if (servable.type == "table") {
-                    generateTable(servable.content)
+                    showData(servable.content)
                 } else {
                     console.log("Unrecognized response: " + response);
                 }
@@ -46,49 +46,4 @@ function generateSingleChart(chart) {
     generateChartDiv(document.getElementById("content"), "chart")
     var chart = c3.generate(chart);
 
-}
-
-function generateTable(stats) {
-
-    function generateTableSkeleton(root, id) {
-        var table = document.createElement('table');
-        table.setAttribute("id", id);
-        var tableHead = document.createElement('thead');
-        var tableBody = document.createElement('tbody');
-        table.appendChild(tableHead);
-        table.appendChild(tableBody);
-        root.appendChild(table);
-    }
-
-    function generatePCVis(root, id) {
-        var div = document.createElement('div');
-        div.setAttribute("id", id);
-        div.setAttribute("class", 'parcoords');
-        div.setAttribute("style", 'height:350px');
-        root.appendChild(div);
-    }
-
-    generatePCVis(document.getElementById("content"), "pcvis")
-
-    var parcoords = d3.parcoords()("#pcvis")
-        .data(stats)
-        .alpha(0.5)
-        .composite("darker")
-        .reorderable()
-        .interactive()
-        .render()
-        .brushMode("1D-axes");
-
-    generateTableSkeleton(document.getElementById("content"), "table")
-
-    var tableHead = d3.select("thead").selectAll("th")
-        .data(d3.keys(stats[0]))
-        .enter().append("th").text(function(key){ return key });
-    var tr = d3.select("tbody").selectAll("tr")
-        .data(stats).enter().append("tr");
-
-    var td = tr.selectAll("td")
-      .data(function(rows){ return d3.values(rows) })
-      .enter().append("td")
-      .text(function(value){ return value });
 }
