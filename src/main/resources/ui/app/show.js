31f7f74b3798dd135bd630f84db444dcbb04596b
milosk <milos.krstajic@gmail.com>
2015-03-19 17:39:13 +0100

    Parallel coordinates with table, linking and brushing
    - first version
    - improve styles and positioning in the next iteration

---
+++ src/main/resources/ui/app/show.js
@@ -0,0 +1,110 @@
+
+
+function showData(data) {
+
+    function generatePCVis(root, id) {
+        var div = document.createElement('div');
+        div.setAttribute("id", id);
+        div.setAttribute("class", 'parcoords');
+        div.setAttribute("style", 'height:350px');
+        root.appendChild(div);
+        var grid = document.createElement('div');
+        grid.setAttribute("id", 'grid');
+        root.appendChild(grid);
+        var pager = document.createElement('div');
+        pager.setAttribute("id", 'pager');
+        root.appendChild(pager);
+    }
+
+    generatePCVis(document.getElementById("content"), "pcvis")
+
+	// slickgrid needs each data element to have an id
+	data.forEach(function(d,i) { d.id = d.id || i; });
+
+
+	var parcoords = d3.parcoords() ("#pcvis")
+		.data(data)
+		.render()
+		.reorderable()
+		.brushMode("1D-axes");
+
+	// setting up grid
+	var column_keys = d3.keys(data[0]);
+	var columns = column_keys.map(function(key,i) {
+		return {
+			id: key,
+			name: key,
+			field: key,
+			sortable: true
+		}
+	});
+
+	var options = {
+			enableCellNavigation: true,
+			enableColumnReorder: false,
+			multiColumnSort: false
+	};
+
+	var dataView = new Slick.Data.DataView();
+	var grid = new Slick.Grid("#grid", dataView, columns, options);
+	var pager = new Slick.Controls.Pager(dataView, grid, $("#pager"));
+
+	// wire up model events to drive the grid
+	dataView.onRowCountChanged.subscribe(function (e, args) {
+		grid.updateRowCount();
+		grid.render();
+	});
+
+	dataView.onRowsChanged.subscribe(function (e, args) {
+		grid.invalidateRows(args.rows);
+		grid.render();
+	});
+
+	// column sorting
+
+	var sortcol = column_keys[0];
+	var sortdir = 1;
+
+	function comparer(a,b) {
+		var x = a[sortcol], y = b[sortcol];
+		return (x == y ? 0 : (x > y ? 1 : -1));
+	}
+
+	// click header to sort grid column
+	grid.onSort.subscribe(function (e, args) {
+		sortdir = args.sortAsc ? 1 : -1;
+		sortcol = args.sortCol.field;
+
+		if ($.browser.msie && $.broswer.version <= 8) {
+			dataView.fastSort(sortcol, args.sortAsc);
+		} else {
+			dataView.sort(comparer, args.sortAsc);
+		}
+	});
+
+	// highlight row in chart
+	grid.onMouseEnter.subscribe(function(e,args) {
+		var i = grid.getCellFromEvent(e).row;
+		var d = parcoords.brushed() || data;
+		parcoords.highlight([d[i]]);
+	});
+	grid.onMouseLeave.subscribe(function(e,args) {
+		parcoords.unhighlight();
+	});
+
+	// fill grid with data
+	gridUpdate(data);
+
+	// update grid on brush
+	parcoords.on("brush", function(d) {
+		gridUpdate(d);
+	});
+
+	function gridUpdate(data) {
+		dataView.beginUpdate();
+		dataView.setItems(data);
+		dataView.endUpdate();
+	};
+
+}
+
