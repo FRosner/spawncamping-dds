a4ccb77b9e5cf74acbe8dc6f85da694205f09a51
Frank Rosner <frank@fam-rosner.de>
2015-04-24 11:58:59 +0200

    #130 allow choice between case class and schema RDD

--- src/main/scala/de/frosner/dds/datasets/package.scala
+++ src/main/scala/de/frosner/dds/datasets/package.scala
@@ -1,6 +1,7 @@
 package de.frosner.dds
 
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{SchemaRDD, SQLContext}
 import org.apache.spark.sql.catalyst.expressions.Row
 import org.apache.spark.sql.catalyst.types._
@@ -9,9 +10,28 @@ import scala.io.Source
 
 package object datasets {
 
-  def golf(implicit sc: SparkContext, sql: SQLContext): SchemaRDD = {
+  private lazy val readGolf = {
     val raw = Source.fromInputStream(this.getClass.getResourceAsStream("/data/golf.csv")).getLines().toSeq
     val (Seq(rawHead), rawBody) = raw.splitAt(1)
+    (rawHead, rawBody)
+  }
+
+  def golf(implicit sc: SparkContext): RDD[GolfRow] = {
+    val (rawHead, rawBody) = readGolf
+    sc.parallelize(rawBody).map(line => {
+      val split = line.split(",", -1)
+      GolfRow(
+        outlook = split(0),
+        temperature = split(1).toDouble,
+        humidity = split(2).toDouble,
+        wind = split(3).toBoolean,
+        play = split(4) == "yes"
+      )
+    })
+  }
+
+  def golf(implicit sc: SparkContext, sql: SQLContext): SchemaRDD = {
+    val (rawHead, rawBody) = readGolf
     val schema = rawHead.split(",", -1).map(columnName => StructField(
       name = columnName,
       dataType = columnName match {
@@ -30,4 +50,17 @@ package object datasets {
     sql.applySchema(data, StructType(schema))
   }
 
+}
+
+case class GolfRow(outlook: String, temperature: Double, humidity: Double, wind: Boolean, play: Boolean) {
+  // override equals because default implementation using pattern matching does not work in Spark REPL
+  override def equals(thatAny: Any): Boolean = {
+    if (thatAny.isInstanceOf[GolfRow]) {
+      val that = thatAny.asInstanceOf[GolfRow]
+      this.outlook == that.outlook && this.temperature == that.temperature && this.humidity == that.humidity &&
+        this.wind == that.wind && this.play == that.play
+    } else {
+      false
+    }
+  }
 }
--- src/test/scala/de/frosner/dds/datasets/DatasetsTest.scala
+++ src/test/scala/de/frosner/dds/datasets/DatasetsTest.scala
@@ -1,5 +1,6 @@
 package de.frosner.dds.datasets
 
+import de.frosner.dds.GolfRow
 import org.apache.spark.sql.catalyst.expressions.Row
 import org.apache.spark.sql.catalyst.types._
 import org.apache.spark.{SparkConf, SparkContext}
@@ -23,7 +24,7 @@ class DatasetsTest extends FlatSpec with Matchers with BeforeAndAfterAll {
     sc.stop()
   }
 
-  "Golf CSV" should "have the correct schema" in {
+  "Golf CSV SchemaRDD" should "have the correct schema" in {
     golf(sc, sql).schema shouldBe StructType(List(
       StructField("Outlook", StringType, false),
       StructField("Temperature", DoubleType, false),
@@ -49,6 +50,25 @@ class DatasetsTest extends FlatSpec with Matchers with BeforeAndAfterAll {
       Row("overcast",72,90,true,true),
       Row("overcast",81,75,false,true),
       Row("rain",71,80,true,false)
+    )
+  }
+
+  "Golf CSV case class RDD" should "have the correct data" in {
+    golf(sc).collect shouldBe Array(
+      GolfRow("sunny",85,85,false,false),
+      GolfRow("sunny",80,90,true,false),
+      GolfRow("overcast",83,78,false,true),
+      GolfRow("rain",70,96,false,true),
+      GolfRow("rain",68,80,false,true),
+      GolfRow("rain",65,70,true,false),
+      GolfRow("overcast",64,65,true,true),
+      GolfRow("sunny",72,95,false,false),
+      GolfRow("sunny",69,70,false,true),
+      GolfRow("rain",75,80,false,true),
+      GolfRow("sunny",75,70,true,true),
+      GolfRow("overcast",72,90,true,true),
+      GolfRow("overcast",81,75,false,true),
+      GolfRow("rain",71,80,true,false)
     )
   }
 
