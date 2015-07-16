bda956fab49133369748073e3a5fcb41736ac3d6
Frank Rosner <frank@fam-rosner.de>
2015-07-16 18:13:59 +0200

    #106 Visualization.js tests
    some of the functionality cannot be tested with the current
    implementation... I have to fix this later

--- src/test/resources/ui/test.dependencies.js
+++ src/test/resources/ui/test.dependencies.js
@@ -1,3 +1,4 @@
 EnvJasmine.loadGlobal(EnvJasmine.rootDir + "util.js");
+EnvJasmine.loadGlobal(EnvJasmine.rootDir + "Visualization.js");
 
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "underscore.min.js");
