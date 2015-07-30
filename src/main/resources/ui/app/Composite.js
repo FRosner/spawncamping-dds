1691259f93137230687397d5fc9c6d56a6eebb7d
Frank Rosner <frank@fam-rosner.de>
2015-07-30 17:05:02 +0200

    #213 avoid floating heights because it destroys the cell layout

--- src/main/resources/ui/app/Composite.js
+++ src/main/resources/ui/app/Composite.js
@@ -74,7 +74,7 @@ Composite.prototype._draw = function(composite) {
       return cell.contentId;
     })
     .attr("style", function(cell) {
-      return "height: " + cell.contentWidth / 16 * 9 + "px"
+      return "height: " + Math.ceil(cell.contentWidth / 16 * 9) + "px"
     })
     .each(function(cell) {
       var servable = cell;
