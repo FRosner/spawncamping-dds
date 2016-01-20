0eed271dc0bf52c58a04230bceb3749aa5679e41
Frank Rosner <frank@fam-rosner.de>
2016-01-20 08:41:39 +0100

    #282 upgrade to REPL Helper 1.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   // Versions
   lazy val sparkVersion = "1.5.0"
   lazy val sprayVersion = "1.3.2"
-  lazy val replHelperVersion = "1.0.2"
+  lazy val replHelperVersion = "1.1.0"
 
   // Dependencies
   val sparkDependencies = Seq(
